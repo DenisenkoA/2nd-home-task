9a01db22ba7239c09e9125a5b2c4f5aec8e1b309
DenisenkoA <49762996+DenisenkoA@users.noreply.github.com>
2019-04-22 21:14:07 +0300

    Add files via upload

---
+++ str_n.py
@@ -0,0 +1,14 @@
+def fun(s,n):
+      if len(s)>n:
+          return s.upper()
+      else:
+          return s
+        
+s=input("Введите строку\n")
+n=int(input("Введите целое число\n"))
+
+print(fun(s,n))
+
+      
+    
+      
